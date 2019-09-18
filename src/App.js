e09ba363a3bad28a1fa92ccacb2be679d09ba7e9
Raul Camacho <raul.camacho@wpengine.com>
2019-09-18 15:25:38 -0500

    add beer stats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import liveOak from './live-oak-hef.png';
 import './App.css';
 
+import BeerStats from './components/BeerStats';
+
 function App() {
   return (
     <div id="wrapper">
@@ -12,6 +14,11 @@ function App() {
           <span className="avatar"><img src={liveOak} alt="" style={{ height: '175px' }}/></span>
           <p><i>"Authentically brewed using a traditional yeast strain that naturally creates the signature flavors of vanilla, clove, and banana."</i></p>
         </header>
+        <BeerStats>
+          <span className="stat"><b>OG: 13&#176;P</b></span>
+          <span className="stat"><b>ABV: 5.3%</b></span>
+          <span className="stat"><b>IBU: 10</b></span>
+        </BeerStats>
       </section>
     </div>
   );
