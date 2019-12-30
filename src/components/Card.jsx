97c392c735831bc01d529b94f0cc33fd1cf2eb9c
Raul Camacho <rcamacho96@gmail.com>
2019-12-29 21:59:22 -0600

    add updated stamps, with older beers too

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 
-const Card = ({ header, image, title, description, stats }) => (
+const Card = ({ image }) => (
   <div className="card">
-    <div className="card-header">
-      <p className="card-header-title is-centered">{header}</p>
-    </div>
     <div className="card-image"><img src={image} alt=""/></div>
-    <div className="card-content is-centered">
-      <p className="title is-centered">{title}</p>
-      <p className="is-2"><i>{description}</i></p>
-      <div className="card-footer is-centered">
-        {
-          stats.map(stat => <div key={stat}><b>{stat}</b></div>)
-        }
-      </div>
-    </div>
   </div>
 )
 
