1a06ced53b8eb7b40d1bf9537311116628612939
Raul Camacho <rcamacho96@gmail.com>
2019-12-30 15:11:32 -0600

    first pass at generalizing beer-adding process

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,20 @@ import React from 'react';
 import styles from './App.module.scss';
 import 'bulma/css/bulma.css';
 
-import flavorCountry from './beer_assets/flavor-country.png';
-import zoe from './beer_assets/zoe.png';
-import elecFish from './beer_assets/elec-fish.png';
-import liveOakHef from './beer_assets/live-oak-hef.png';
-
+import beerData from './beer-data';
 
 const App = () => (
   <div className={styles.wrapper}>
     <h1>11th Floor Kegerator</h1>
     <hr/>
     <div className="columns is-multiline">
-      <div className="column">
-        <img src={flavorCountry} alt="Flavor Country" style={{ 'maxWidth': '300px' }} />
-      </div>
-      <div className="column">
-        <img src={zoe} alt="Zoe" style={{ 'maxWidth': '300px' }} />
-      </div>
-      <div className="column">
-        <img src={elecFish} alt="Electric Jellyfish" style={{ 'maxWidth': '300px' }} />
-      </div>
-      <div className="column">
-        <img src={liveOakHef} alt="Live Oak Hef" style={{ 'maxWidth': '300px' }} />
-      </div>
+      {
+        beerData.map(beer => (
+          <div className="column">
+            <img src={beer.imagePath} alt={beer.name} style={{ 'maxWidth': '300px' }} />
+          </div>
+        ))
+      }
     </div>
   </div>
 );
---
+++ src/beer-data.js
@@ -0,0 +1,25 @@
+import flavorCountry from './beer_assets/flavor-country.png';
+import zoe from './beer_assets/zoe.png';
+import electricJellyfish from './beer_assets/elec-fish.png';
+import liveOakHef from './beer_assets/live-oak-hef.png';
+
+const beerData = [
+  {
+    name: 'Austin Beerworks Flavor Country',
+    imagePath: flavorCountry,
+  },
+  {
+    name: 'Hops & Grains The One They Call Zoe',
+    imagePath: zoe,
+  },
+  {
+    name: 'PHP Electric Jellyfish',
+    imagePath: electricJellyfish,
+  },
+  {
+    name: 'Live Oak Hefeweizen',
+    imagePath: liveOakHef,
+  },
+];
+
+export default beerData;
